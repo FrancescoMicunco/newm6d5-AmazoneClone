fc686ef8ece67bebbf68182d78c02450f9402ccb
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-17 15:33:22 +0100

    router for products and reviews

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -1,31 +1,97 @@
 import express from "express";
 import { Op, Sequelize } from "sequelize";
+import Products from "../../utils/models/products.js";
 
 const router = express.Router();
 
 router
     .route("/")
     .get(async(req, res, next) => {
-        try {} catch (error) {}
+        try {
+            const product = await Products.findAll({
+                where: {
+                    ...(req.query.search && {
+                        [Op.or]: [{
+                                name: {
+                                    [Op.iLike]: `%${req.query.search}%`,
+                                },
+                            },
+                            {
+                                lastName: {
+                                    [Op.iLike]: `%${req.query.search}%`,
+                                },
+                            },
+                            {
+                                email: {
+                                    [Op.iLike]: `%${req.query.search}%`,
+                                },
+                            },
+                            {
+                                country: {
+                                    [Op.iLike]: `%${req.query.search}%`,
+                                },
+                            },
+                        ],
+                    }),
+                },
+                // here eventually join table
+                //===================
+            });
+            res.send(product);
+        } catch (error) {
+            next(error);
+        }
     })
     .post(async(req, res, next) => {
-        try {} catch (error) {}
+        try {
+            const product = await Products.create(req.body);
+            res.send(product);
+        } catch (error) {
+            next(error);
+        }
     });
 
 router
-    .route("/bulk")
+    .route("/:id")
     .get(async(req, res, next) => {
-        try {} catch (error) {}
+        try {
+            const product = await Products.findOne({
+                where: {
+                    id: req.params.id,
+                },
+                //===== here eventually join
+            });
+            res.send(product);
+        } catch (error) {
+            next(error);
+        }
     })
-    .post(async(req, res, next) => {
-        try {} catch (error) {}
-    })
-    .put(async(req, res, next) => {
-        try {} catch (error) {}
-    })
-    .delete(async(req, res, next) => {
-        try {} catch (error) {}
-    });
 
+.put(async(req, res, next) => {
+    try {
+        const updateProduct = await Products.update(req.body, {
+            where: { id: req.params.id },
+            returning: true,
+        });
+        res.send(updateProduct);
+    } catch (error) {
+        next(error);
+    }
+})
+
+.delete(async(req, res, next) => {
+    try {
+        const deletedProduct = await Products.destroy({
+            where: { id: req.params.id },
+        });
+        if (deletedProduct > 0) {
+            res.send("201. producte deleted");
+        } else {
+            ("product not found!");
+        }
+    } catch (error) {
+        next(error);
+    }
+});
 
 export default router
--- src/services/reviews/index.js
+++ src/services/reviews/index.js
@@ -1,31 +1,98 @@
 import express from "express";
 import { Op, Sequelize } from "sequelize";
+import Reviews from "../../utils/models/reviews.js";
 
 const router = express.Router();
 
 router
     .route("/")
     .get(async(req, res, next) => {
-        try {} catch (error) {}
+        try {
+            const reviews = await Reviews.findAll({
+                where: {
+                    ...(req.query.search && {
+                        [Op.or]: [{
+                                name: {
+                                    [Op.iLike]: `%${req.query.search}%`,
+                                },
+                            },
+                            {
+                                lastName: {
+                                    [Op.iLike]: `%${req.query.search}%`,
+                                },
+                            },
+                            {
+                                email: {
+                                    [Op.iLike]: `%${req.query.search}%`,
+                                },
+                            },
+                            {
+                                country: {
+                                    [Op.iLike]: `%${req.query.search}%`,
+                                },
+                            },
+                        ],
+                    }),
+                },
+                // here eventually join table
+                //===================
+            });
+            res.send(reviews);
+        } catch (error) {
+            next(error);
+        }
     })
     .post(async(req, res, next) => {
-        try {} catch (error) {}
+        try {
+            const reviews = await Reviews.create(req.body);
+            res.send(reviews);
+        } catch (error) {
+            next(error);
+        }
     });
 
 router
-    .route("/bulk")
+    .route("/:id")
     .get(async(req, res, next) => {
-        try {} catch (error) {}
+        try {
+            const reviews = await Reviews.findOne({
+                where: {
+                    id: req.params.id,
+                },
+                //===== here eventually join
+            });
+            res.send(reviews);
+        } catch (error) {
+            next(error);
+        }
     })
-    .post(async(req, res, next) => {
-        try {} catch (error) {}
-    })
-    .put(async(req, res, next) => {
-        try {} catch (error) {}
-    })
-    .delete(async(req, res, next) => {
-        try {} catch (error) {}
-    });
+
+.put(async(req, res, next) => {
+    try {
+        const updateReviews = await Reviews.update(req.body, {
+            where: { id: req.params.id },
+            returning: true,
+        });
+        res.send(updateReviews);
+    } catch (error) {
+        next(error);
+    }
+})
+
+.delete(async(req, res, next) => {
+    try {
+        const deletedReviews = await Reviews.destroy({
+            where: { id: req.params.id },
+        });
+        if (deletedReviews > 0) {
+            res.send("201. reviewse deleted");
+        } else {
+            ("reviews not found!");
+        }
+    } catch (error) {
+        next(error);
+    }
+});
 
 
 
